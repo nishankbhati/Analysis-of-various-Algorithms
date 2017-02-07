e815d9fcbf7f6f384b3783271d821122b9109be3
Pradyumn Agrawal <pradyumnag1997@gmail.com>
2017-02-07 19:42:19 +0530

    Number of comparisons count of both algorithm.

--- Hoare's Partition Quicksort.java
+++ Hoare's Partition Quicksort.java
@@ -2,57 +2,104 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 
-public class Hoares_Quick_Sort_Imple{
+public class Quick_Sort_Implementation{
     public static PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
-    public static long iter = 0;
 
     public static void main(String[] args) throws java.lang.Exception {
-        new Quick_Sort_Imple().run();
+        new Quick_Sort_Implementation().run();
         out.close();
     }
     void run() throws java.lang.Exception {
+        long comparisons_by_Normal_partition = 0;
+        long comparisons_by_Hoares_partition = 0;
+
         Random gen = new Random();
         int n = 100;
         int[] a = new int[n];
         for(int i = 0;i < n; i++){
             a[i] = gen.nextInt(10000)%1000;
         }
-        out.println(Arrays.toString(a));
-        QuickSort(a,0,n - 1);
-        out.println(Arrays.toString(a));
-        out.println("Iterations by Hoare's Algorithm -> " + iter);
+        //out.println(Arrays.toString(a));
+        int[] b = new int[n];
+        System.arraycopy(a, 0, b, 0, n);
+        comparisons_by_Hoares_partition = QuickSort_Hoares(a,0,n - 1);
+        comparisons_by_Normal_partition = QuickSort_Normal(b,0,n - 1);
+        out.println("Comparisons by Hoare's Partition Algorithm -> " + comparisons_by_Hoares_partition);
+        out.println("Comparisons by Normal Partition Algorithm -> " + comparisons_by_Normal_partition);
     }
-    public static void QuickSort(int[] a, int p, int r) {
+    public static long QuickSort_Hoares(int[] a, int p, int r) {
+        long comparisons_by_Hoares_partition = 0;
         if(p < r){
-            int q = H_Partition(a,p,r);
-            QuickSort(a,p,q);
-            QuickSort(a,q + 1,r);
+            long[] val = Hoares_Partition(a,p,r);
+            int q = (int)val[0];
+            comparisons_by_Hoares_partition += val[1];
+            comparisons_by_Hoares_partition += QuickSort_Hoares(a,p,q);
+            comparisons_by_Hoares_partition += QuickSort_Hoares(a,q + 1,r);
         }
+        return comparisons_by_Hoares_partition;
     }
-    private static int H_Partition(int[] a, int p, int r) {
+    private static long[] Hoares_Partition(int[] a, int p, int r) {
 
         int pivot = a[p];
         int i = p - 1;
         int j = r + 1;
-
+        long comparisons_by_Hoares_partition = 0;
         while (true) {
             do {
                 i++;
-                iter++;
+                comparisons_by_Hoares_partition++;
             } while (a[i] < pivot);
 
             do {
                 j--;
-                iter++;
+                comparisons_by_Hoares_partition++;
             } while (a[j] > pivot);
 
             if (i >= j) {
-                return j;
+                return new long [] {j,comparisons_by_Hoares_partition};
             } else {
                 a[i] ^= a[j];
                 a[j] ^= a[i];
                 a[i] ^= a[j];
             }
         }
+    }
+    public static long QuickSort_Normal(int[] a, int p, int r) {
+        long comparisons_by_Normal_partition = 0;
+        long[] val = partition_Of_Normal_QuickSort(a, p, r);
+        int ind = (int)val[0];
+        comparisons_by_Normal_partition += val[1];
+        if(p < ind - 1){
+            comparisons_by_Normal_partition += QuickSort_Normal(a, p, ind - 1);
+        }
+        if(ind < r){
+            comparisons_by_Normal_partition += QuickSort_Normal(a, ind, r);
+        }
+        return comparisons_by_Normal_partition;
+    }
+    public static long[] partition_Of_Normal_QuickSort(int[] a, int p, int r) {
+        long comparisons_by_Normal_partition = 0;
+        int i = p;
+        int j = r;
+        int pivot = a[(p + r) >>> 1];
+        while(i <= j){
+            while(a[i] < pivot){
+                comparisons_by_Normal_partition++;
+                i++;
+            }
+            while(a[j] > pivot){
+                comparisons_by_Normal_partition++;
+                j--;
+            }
+            if(i <= j){
+                comparisons_by_Normal_partition++;
+                a[i] ^= a[j];
+                a[j] ^= a[i];
+                a[i] ^= a[j];
+                i++;
+                j--;
+            }
+        }
+        return new long[] {i,comparisons_by_Normal_partition};
     }
 }
